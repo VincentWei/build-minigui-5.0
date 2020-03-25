a6101ccdc19920795c58c72f3f7cd32d6db61b86
Vincent Wei <vincent@minigui.org>
2020-03-25 10:50:00 +0800

    add switch-to-release-branch.sh; rename switch-branch.sh to switch-to-master-branch.sh

---
+++ switch-to-release-branch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+branch=rel-5-0
+
+if [ ! -f myconfig.sh ]; then
+    cp config.sh myconfig.sh
+fi
+
+source myconfig.sh
+
+for comp in minigui-res mg-samples minigui mgutils mgplus mgeff mgncs mgncs4touch mg-tests mg-demos; do
+    echo "checkout branch $branch for $comp..."
+    cd $comp
+    git branch $branch && git pull && git checkout $branch
+    cd ..
+done
+
+# gvfb, mg-tools, and cell-phone-ux-demo are always in master

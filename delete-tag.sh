40be123b1f4a667203c486fac47c79c34edd307c
Vincent Wei <vincent@minigui.org>
2021-05-19 11:04:08 +0800

    delete-tag.sh

---
+++ delete-tag.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+tag=ver-5-0-6
+
+if [ ! -f myconfig.sh ]; then
+    cp config.sh myconfig.sh
+fi
+
+source myconfig.sh
+
+for comp in minigui-res mg-samples minigui mgutils mgplus mgeff mgncs mgncs4touch mg-tests mg-demos cell-phone-ux-demo; do
+    echo "DELETING TAG ON $comp..."
+    cd $comp
+    git tag -d $tag
+    git push origin :refs/tags/$tag
+    cd ..
+done

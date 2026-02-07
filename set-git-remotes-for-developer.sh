16f75fb59247ada0c199eb10f9203c2f46698a96
Vincent Wei <vincent@minigui.org>
2026-02-07 12:41:52 +0800

    update

--- set-git-remotes-for-developer.sh
+++ set-git-remotes-for-developer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for comp in minigui-res mg-samples minigui mgutils mgplus mgeff mgncs mgncs4touch mg-tests mg-demos cell-phone-ux-demo; do
+for comp in minigui-res mg-samples minigui mg-tools mgutils mgplus mgeff mgncs mgncs4touch mg-tests mg-demos cell-phone-ux-demo; do
     echo
     echo "CHECKING STATUS IN $comp..."
     cd $comp
